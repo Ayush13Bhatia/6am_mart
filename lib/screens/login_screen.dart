cb72e9d08cc4f17da224d596ab6e3d3f9d8b7274
Ayush13Bhatia <ayushbhatia084@gmail.com>
2023-02-21 14:45:42 +0530

    login page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 
+import '../components/my_text.dart';
+import '../utils/images_url.dart';
+import '../utils/my_theme.dart';
+
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("title"),
-      ),
       body: Column(
         children: [
-          Text('data'),
+          Image.asset(
+            ImageUrl.assetsImageParcel,
+            height: size.height * 0.3,
+            width: double.maxFinite,
+          ),
+          const SizedBox(height: 20),
+
+          ///Text Your Phone Number
+          const MyText(
+            "Your Phone Number",
+            textAlign: TextAlign.center,
+            textStyle: TextStyle(
+              color: MyTheme.primary,
+              fontSize: 22,
+              fontFamily: "Roboto",
+            ),
+          ),
+          const SizedBox(height: 20),
         ],
       ),
     );
