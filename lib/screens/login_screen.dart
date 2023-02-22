f6d471e32f29b532f9abff98d87514a264784533
Ayush13Bhatia <ayushbhatia084@gmail.com>
2023-02-22 16:19:51 +0530

    login page text form field

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../components/my_text.dart';
+import '../components/my_text_field.dart';
 import '../utils/images_url.dart';
 import '../utils/my_theme.dart';
 
@@ -31,6 +32,45 @@ class LoginScreen extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 20),
+          const SizedBox(height: 20),
+          MyTextField(
+            controller: _countryController,
+            label: "Country/Region",
+            isReadOnly: true,
+            // prefixWidget: ,
+            validator: (st) {
+              if (st == null) {
+                return "Please Select Country";
+              }
+              if (st.isEmpty) {
+                return "Please Select Country";
+              }
+              return null;
+            },
+            suffixWidget: const Padding(
+              padding: EdgeInsets.only(right: 10),
+              child: Icon(
+                Icons.arrow_drop_down,
+                color: MyTheme.primary,
+              ),
+            ),
+            onTap: () {
+              showCountryPicker(
+                context: context,
+                showPhoneCode: true,
+                favorite: ["IN"],
+                countryListTheme: CountryListThemeData(
+                  flagSize: 35,
+                  bottomSheetHeight: size.height * 0.8,
+                ),
+                onSelect: (Country country) {
+                  print('Select country: ${country.flagEmoji}');
+                  _countryController.text = "${country.flagEmoji} ${country.name}(+${country.phoneCode})";
+                },
+              );
+            },
+          ),
+          const SizedBox(height: 2),
         ],
       ),
     );
