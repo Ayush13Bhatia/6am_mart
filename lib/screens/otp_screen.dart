348d34cc0205d6afacafbc6a591467bdc92c2e48
Ayush13Bhatia <ayushbhatia084@gmail.com>
2023-03-03 22:39:58 +0530

    focus node

--- lib/screens/otp_screen.dart
+++ lib/screens/otp_screen.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class OTPScreen extends StatelessWidget {
   const OTPScreen({Key? key}) : super(key: key);
 
+  final pinController = TextEditingController();
+  final focusNode = FocusNode();
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
